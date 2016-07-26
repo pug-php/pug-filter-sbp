012cfc21fa5842e53df4fd4ed7c31bb2d40ac0a6
Kyle <kylekatarnls@users.noreply.github.com>
2016-07-26 12:17:49 +0200

    Simplify thanks to Pug-php 2

--- src/Pug/Filter/Sbp.php
+++ src/Pug/Filter/Sbp.php
@@ -2,21 +2,12 @@
 
 namespace Pug\Filter;
 
-use Jade\Compiler;;
-use Jade\Filter\AbstractFilter;
-use Jade\Nodes\Filter;
 use Sbp\Sbp as SbpParser;
 
 class Sbp extends AbstractFilter
 {
-    public function __invoke(Filter $node, Compiler $compiler)
+    public function parse($sbp)
     {
-        $nodes = $node->block->nodes;
-        $indent = strlen($nodes[0]->value) - strlen(ltrim($nodes[0]->value));
-        $sbp = '<?';
-        foreach ($nodes as $line) {
-            $sbp .= "\n" . substr($line->value, $indent);
-        }
         $php = SbpParser::parse($sbp);
 
         return $php . (preg_match('`\?>\s*$`', $php) ? '' : '?>');
