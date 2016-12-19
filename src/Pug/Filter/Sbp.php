59a523d4f9e9eca206e8ac6728ba706f9ad69442
kylekatarnls <kylekatarnls@gmail.com>
2016-12-19 16:52:34 +0100

    Complete with <? if missing

--- src/Pug/Filter/Sbp.php
+++ src/Pug/Filter/Sbp.php
@@ -8,6 +8,9 @@ class Sbp extends AbstractFilter
 {
     public function parse($sbp)
     {
+        if (!preg_match('`^\s*<\?`', $sbp)) {
+            $sbp = "<?\n" . $sbp;
+        }
         $php = SbpParser::parse($sbp);
 
         return $php . (preg_match('`\?>\s*$`', $php) ? '' : '?>');
